8c48dc80eb0e1da145890924fc804c5c17ee767b
beccasbound <rebecca.s.peabody@gmail.com>
2016-07-17 14:55:18 -0500

    update evaluator

--- DS_P2Main.cpp
+++ DS_P2Main.cpp
@@ -2,6 +2,8 @@
 #include<string>
 #include"Tokenizer.h"
 #include"Token.h"
+#include "Evaluator.h"
+
 
 using namespace std;
 
@@ -12,12 +14,20 @@ using namespace std;
 //}
 int main()
 {
-	Tokenizer obj(string(")%6+--2"));
- /*   while (obj.has_more_tokens()) {
-        obj.next_token();
-    }*/
+    string expression = "2+3";
 
-	
+ /*   Tokenizer parser(expression);
+
+    do {
+        parser.next_token();
+    } while (parser.has_more_tokens());*/
+
+
+    Evaluator eval;
+    int result = eval.eval(expression);
+    cout << "result is " << result << endl;
+
+
     system("pause");
 
 }
